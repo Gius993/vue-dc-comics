2e7e2b96dda993937dd55836b5b40834724f9ed0
Gius993 <giuseppevidili9@gmail.com>
2022-07-06 17:51:44 +0200

    header

--- project/src/App.vue
+++ project/src/App.vue
@@ -1,28 +1,26 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <!-- <img alt="Vue logo" src="./assets/logo.png">
+    <HelloWorld msg="Welcome to Your Vue.js App"/> -->
+    
+  <PageHeader />
+ 
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+   import PageHeader from "./components/PageHeader.vue";
+// import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components:{
+    PageHeader
+}
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  @import './style/common.scss';
+  @import  './style/variables.scss';
 </style>
---
+++ project/src/components/PageHeader.vue
@@ -0,0 +1,109 @@
+<template>
+	<div>
+		<div class="container-header">
+			<!--logo-->
+			<div class="logo">
+				<img src="../assets/img/dc-logo.png" alt="logo">
+			</div>
+			<!--list-->
+			<div class="list-branding">
+				<ul>
+					<li v-for="link, index in menuList" :key="index">
+						<a href="#">{{ link.text }}</a>
+					</li>									
+				</ul>
+			</div>
+
+		</div>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "PageHeader",
+		data(){
+			return{
+				menuList:[
+					{
+						text:'	CHARACTERS',
+						url: '#',
+						current: true
+					},
+						{
+						text:'COMIC',
+						url: '#',
+						current: true
+					},
+						{
+						text:'MOVIES',
+						url: '#',
+						current: true
+					},
+						{
+						text:'TV',
+						url: '#',
+						current: true
+					},
+						{
+						text:'GAMES',
+						url: '#',
+						current: true
+					},
+						{
+						text:'COLLECTIBLE',
+						url: '#',
+						current: true
+					},
+						{
+						text:'VIDEO',
+						url: '#',
+						current: true
+					},
+						{
+						text:'FANS',
+						url: '#',
+						current: true
+					},
+						{
+						text:'NEWS',
+						url: '#',
+						current: true
+					},
+						{
+						text:'SHOP',
+						url: '#',
+						current: true
+					},
+				]
+			}
+		}
+  }
+
+</script>
+
+<style lang="scss">
+.logo{
+	width: 100px;
+	img{
+		padding: 10px;
+	}
+}
+.list-branding{
+	a{
+		color: black;		
+	}
+	ul{
+		display: flex;		
+	}
+	li{
+		padding-left: 20px;
+		font-size: 16px;
+	}
+}
+.container-header{
+	height: 300px;
+	display: flex;
+	justify-content: space-around;
+	align-items: center;
+}
+</style>
