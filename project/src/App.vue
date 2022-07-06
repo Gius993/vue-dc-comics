dd10f822c62f2ce6a50b0693900cfabffd88fa5f
Gius993 <giuseppevidili9@gmail.com>
2022-07-06 18:08:15 +0200

    inizio main

--- project/src/App.vue
+++ project/src/App.vue
@@ -2,20 +2,21 @@
   <div id="app">
     <!-- <img alt="Vue logo" src="./assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/> -->
-    
   <PageHeader />
- 
+    <PageMain />
   </div>
 </template>
 
 <script>
    import PageHeader from "./components/PageHeader.vue";
+   import PageMain from "./components/PageMain.vue";
 // import HelloWorld from './components/HelloWorld.vue'
 
 export default {
   name: 'App',
   components:{
-    PageHeader
+    PageHeader,
+    PageMain,
 }
 }
 </script>
---
+++ project/src/components/PageMain.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>	  
+	  <div class="main_class">
+		
+	  </div>
+  </div>
+</template>
+
+<script>
+
+
+</script>
+
+
+<style scoped lang="scss">
+	@import '../style/variables.scss';
+	.main_class{
+		background-color:$branc_color_d;
+	}
+</style>
